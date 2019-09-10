68b1c6d0aa5da3a6c61af8ca2a110eebc4cd64d1
Carson Smith <47376294+GrazeDD@users.noreply.github.com>
2019-09-10 13:18:47 -0700

    added project files
    I added every file I have under my project. I really hope this is the right stuff.

---
+++ Assignment2/Deliverable2/BMICalculator/ViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by user159305 on 9/9/19.
+//  Copyright © 2019 Carson Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var unitSystemSwitch: UISwitch!
+    
+    @IBOutlet weak var unitSwitchLabel: UILabel!
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    
+    @IBOutlet weak var heightTextField: UITextField!
+    
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var weightTextField: UITextField!
+    
+    @IBOutlet weak var calculationButton: UIButton!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var result2Label: UILabel!
+    //This boolean value depicts the type of system that is being used. False meaning metric and true being Standard
+    var unitSystemBoolean:Bool = false;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        
+        view.addGestureRecognizer(tap)
+    }
+    
+    @IBAction func SwitchFlipped(_ sender: Any) { //Change from metric computation to standard or vise-versa
+        
+        if unitSystemSwitch.isOn {
+            unitSwitchLabel.text = "Standard"
+            heightLabel.text = "Height (in)"
+            weightLabel.text = "Weight (lb)"
+            heightTextField.text = ""
+            weightTextField.text = ""
+            unitSystemBoolean = true
+        } else {
+            unitSwitchLabel.text = "Metric"
+            heightLabel.text = "Height (cm)"
+            weightLabel.text = "Weight (kg)"
+            heightTextField.text = ""
+            weightTextField.text = ""
+            unitSystemBoolean = false
+        }
+    }
+    
+    @IBAction func CalculateButtonPressed(_ sender: Any) {
+        
+        let stringHeight = heightTextField.text
+        let stringWeight = weightTextField.text
+        
+        let height:Int = Int(stringHeight!) ?? 0
+        let weight:Int = Int(stringWeight!) ?? 0
+        
+        if unitSystemBoolean {
+            //Standard System
+            //703 times weight(lb) divided by height in inches squared
+            
+            let BMI:Float = (Float(703) * Float(weight)) / (Float(height) * Float(height))
+            resultLabel.text = "BMI = \(BMI)"
+            if BMI < 18.5 {
+                result2Label.text = "You are Underweight"
+            } else if BMI < 25 {
+                result2Label.text = "You are Average Weight"
+            } else if BMI > 25 {
+                result2Label.text = "You are Overweight"
+            }
+            
+        } else {
+            //Metric System
+            //Weight(kg) divided by height in meters squared.
+            let realHeight:Float = Float(height)/Float(100)
+            let BMI:Float = Float(weight) / (Float(realHeight) * Float(realHeight))
+            resultLabel.text = "BMI = \(BMI)"
+            if BMI < 18.5 {
+                result2Label.text = "You are Underweight"
+            } else if BMI < 25 {
+                result2Label.text = "You are Average Weight"
+            } else if BMI > 25 {
+                result2Label.text = "You are Overweight"
+            }
+        }
+        
+    }
+    
+}
